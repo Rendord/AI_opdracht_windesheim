4dd344afa5c81dcffb4dd7b9dfdf2a0f93ba1492
Bas <rendord@gmail.com>
2022-06-24 17:04:32 +0200

    fuck this shit

--- Smart Zombies/Scenes/Character.cs
+++ Smart Zombies/Scenes/Character.cs
@@ -2,25 +2,25 @@ using Godot;
 
 namespace SmartZombies.Scenes
 {
-    public abstract class Character : KinematicBody2D
-    {
-        [Export] protected readonly float MaxSpeed;
+	public abstract class Character : KinematicBody2D
+	{
+		[Export] protected readonly float MaxSpeed;
 
-        [Export] public readonly float Mass;
+		[Export] public readonly float Mass;
 
-        public Vector2 Velocity;
-        public Vector2 Heading;
-        public bool Tagged = false;
+		public Vector2 Velocity;
+		public Vector2 Heading;
+		public bool Tagged = false;
 
-        public override void _PhysicsProcess(float delta)
-        {
-            if (Velocity.LengthSquared() > 0.0001)
-            {
-                Heading = Velocity.Normalized();
-            }
+		public override void _PhysicsProcess(float delta)
+		{
+			if (Velocity.LengthSquared() > 0.0001)
+			{
+				Heading = Velocity.Normalized();
+			} 
 
-            Velocity = Velocity.Clamped(MaxSpeed);
-            MoveAndSlide(Velocity);
-        }
-    }
+			Velocity = Velocity.Clamped(MaxSpeed);
+			MoveAndSlide(Velocity);
+		}
+	}
 }
--- Smart Zombies/Scenes/Zombie/Zombie.cs
+++ Smart Zombies/Scenes/Zombie/Zombie.cs
@@ -5,6 +5,11 @@ namespace SmartZombies.Scenes.Zombie
 {
 	public class Zombie : Character
 	{
+		private RandomNumberGenerator rng = new RandomNumberGenerator();
+		private float _wanderRadius = 5f;
+		private float _wanderJitter = 4f;
+		private Vector2 _wanderPoint;
+		
 		private enum Deceleration
 		{
 			Fast = 1,
@@ -14,22 +19,28 @@ namespace SmartZombies.Scenes.Zombie
 
 		private Character _target;
 		private RayCast2D _velocityRayCast;
+		private RayCast2D _wanderRayCast;
+		private Node2D _circle;
 
 		public override void _Ready()
 		{
+			_wanderPoint = Position;
 			_target = GetTree().CurrentScene.GetNode<Character>("Human");
 			_velocityRayCast = GetNode<RayCast2D>("Velocity");
+			_wanderRayCast = GetNode<RayCast2D>("Wander");
+			_circle = GetNode<Node2D>("Node2D");
 		}
 
 		public override void _PhysicsProcess(float delta)
 		{
 			// Velocity += SeekBehaviour(_target.Position);
-			Velocity += PursuitBehaviour(_target);
-			Velocity += ArriveBehaviour(_target.Position, Deceleration.Fast);
-
+			//Velocity += PursuitBehaviour(_target);
+			//Velocity += ArriveBehaviour(_target.Position, Deceleration.Fast);
+			Velocity += WanderBehaviour(50);
 			_velocityRayCast.CastTo = Velocity;
 
 			base._PhysicsProcess(delta);
+			
 		}
 
 		private Vector2 SeekBehaviour(Vector2 targetPosition)
@@ -72,6 +83,31 @@ namespace SmartZombies.Scenes.Zombie
 			
 			var lookAheadTime = toEvader.Length() / MaxSpeed + evader.Velocity.Length();
 			return SeekBehaviour(evader.Position + evader.Velocity * lookAheadTime);
+		}
+		
+		private Vector2 EvadeBehaviour(Character pursuer)
+		{
+			var toPursuer = pursuer.Position - Position;
+			var LookAheadTime = toPursuer.Length() / MaxSpeed + pursuer.Velocity.Length();
+			return FleeBehaviour(pursuer.Position + pursuer.Velocity * LookAheadTime);
+		}
+		
+		private Vector2 WanderBehaviour(float wanderDistance)
+		{
+			float x = rng.RandfRange(-1, 1) * _wanderJitter;
+			float y = rng.RandfRange(-1, 1) * _wanderJitter;
+
+			var wanderDisplacement = new Vector2(x, y);
+			_wanderPoint += wanderDisplacement;
+			_wanderPoint = _wanderPoint.Normalized();
+			_wanderPoint *= _wanderRadius;
+			_circle.Position = Heading * wanderDistance;
+			var wanderTarget = (Heading * wanderDistance) + _wanderPoint;
+			_wanderRayCast.CastTo = wanderTarget;
+
+			return wanderTarget;
+
+			
 		}
 	}
 }
